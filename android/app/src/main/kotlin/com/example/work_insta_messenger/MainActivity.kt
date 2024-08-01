5e887aebab19f2ab499555fdc6e4adaa0e6d4b31
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-08-01 17:43:57 -0500

    chore: Add local_auth plugin for biometric authentication support

--- android/app/src/main/kotlin/com/example/work_insta_messenger/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/work_insta_messenger/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.work_insta_messenger
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
